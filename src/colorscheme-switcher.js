a23d7c49178e8f2ade158e807b69f9422f8487da
jartigag <jartigag@pm.me>
2020-05-04 01:01:00 +0200

    //todo: fix colorscheme-switcher.js for chromium

--- src/colorscheme-switcher.js
+++ src/colorscheme-switcher.js
@@ -33,7 +33,7 @@ function theme_change_requested(){
 
 function getCurrentColor(){
   // Color was set before in localStorage
-  var storage_color = localStorage.getItem("color");
+  var storage_color = localStorage.getItem("color"); //TODO: fix for chromium
   if(storage_color !== null){
     return storage_color;
   }
